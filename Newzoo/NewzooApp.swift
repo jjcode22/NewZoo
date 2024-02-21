1c54b868c7f209712fb6d1803a2ac222eb98caa4
jjcode22 <jjmcoding31@gmail.com>
2024-02-21 11:48:56 +0530

    Initial Commit

---
+++ Newzoo/NewzooApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewzooApp.swift
+//  Newzoo
+//
+//  Created by JJ on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NewzooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
